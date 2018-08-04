63b05dec79e2a2e4e5e08780c61ab0a12458d87a
ahorawzy <lzyz09193@163.com>
2018-08-04 12:39:33 +0800

    Wirte function save_list_df and its document.

---
+++ R/save_list_df.R
@@ -0,0 +1,19 @@
+#' Write dataframes saved in a list
+#'
+#' \code{save_list_df} writes dataframes saved in a list into certain folder in batch.
+#'
+#' @param objectlist The list contains several dataframes as its elements.
+#' @param path A string of path, ended by "\\\" in windows, like "D:\\\"
+#' @return Write csv files in the certain path and return NULL.
+#'
+#' @example
+#' A = list(df1 = data.frame(a = 1:3,b = letters[1:3]), df2 = data.frame(a = 1:10, b = LETTERS[1:10]))
+#' save_list_df(A, "D:\\")
+#'
+#' @export
+save_list_df <- function(objectlist,path){
+  for(i in 1:length(objectlist)){
+    address <- paste(c(path,format(names(objectlist)[i]),".csv"),collapse = "")
+    write.csv(objectlist[[i]],file=address,na = "",row.names = F)
+  }
+}

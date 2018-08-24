e29abf6fffab6a0e48d19ed32a4573fd0c433eb5
ahorawzy <lzyz09193@163.com>
2018-08-24 21:19:09 +0800

    Amend a small mistake.

--- R/save_list_df.R
+++ R/save_list_df.R
@@ -6,7 +6,7 @@
 #' @param path A string of path, ended by '\\\' in windows, like 'D:\\\'
 #' @return Write csv files in the certain path and return NULL.
 #'
-#' @example
+#' @examples
 #' A = list(df1 = data.frame(a = 1:3,b = letters[1:3]), df2 = data.frame(a = 1:10, b = LETTERS[1:10]))
 #' save_list_df(A, 'D:\\')
 #'

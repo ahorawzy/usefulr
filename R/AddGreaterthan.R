d6f3d6bc48975b1dd9f4b04b840b2dcf091be528
ahorawzy <lzyz09193@163.com>
2019-04-02 16:09:02 +0800

    Add new function.

---
+++ R/AddGreaterthan.R
@@ -0,0 +1,6 @@
+AddGreaterthan <- function(filename,encodestyle){
+  file <- readLines(filename,encoding=encodestyle)
+  noemptyindex <- which(nchar(file)!=0)
+  file[noemptyindex] <- paste(">",file[noemptyindex])
+  writeLines(file,filename)
+}
